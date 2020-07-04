2561589dc7b6a24aadd2dd690e37a8aa99169695
Renato Alencar <renatoalencar.73@gmail.com>
2020-07-03 21:04:08 -0300

    Setup and validate test environment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isVisible, showHello] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={showHello}>click me</button>
+
+      {isVisible && <b>Hello</b>}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('adding a transaction', async () => {
+  const app = render(<App />);
+
+  fireEvent.click(app.getByText(/click me/i));
+
+  expect(app.getByText(/hello/i)).toBeInTheDocument();
 });
