f38ebdc044dbed415b4b48b2e890049fc71a4f5f
Renato Alencar <renatoalencar.73@gmail.com>
2020-07-04 12:57:43 -0300

    Add and validate transactions

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,26 @@ import App from './App';
 test('adding a transaction', async () => {
   const app = render(<App />);
 
-  fireEvent.click(app.getByText(/click me/i));
+  fireEvent.change(app.getByLabelText(/description/i), {
+    target: { value: 'a coffee' },
+  });
+  fireEvent.change(app.getByLabelText(/value/i), {
+    target: { value: '2,75' },
+  });
+  fireEvent.click(app.getByText(/add/i));
 
-  expect(app.getByText(/hello/i)).toBeInTheDocument();
+  expect(app.getByText(/a coffee/)).toBeInTheDocument();
+  expect(app.getByText('R$ 2,75')).toBeInTheDocument();
+});
+
+test('cant add invalid transaction', async () => {
+  const app = render(<App />);
+
+  fireEvent.change(app.getByLabelText(/description/i), {
+    target: { value: 'a coffee' },
+  });
+  /* The value field is left empty */
+  fireEvent.click(app.getByText(/add/i));
+
+  expect(app.queryAllByText('a coffee')).toHaveLength(0);
 });
